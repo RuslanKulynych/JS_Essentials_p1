114c9a6b62a66978af90350ed96bad67e73f9e9d
Ruslan Kulynych <ruslan.kulynych8@gmail.com>
2017-05-17 07:56:41 +0300

    deleted readme.md

---
+++ script.js
@@ -0,0 +1,6 @@
+var time = "16:30";
+var place = "Lviv";
+var person = prompt("Please enter your name", "name");
+var event = prompt("enter event", "event");
+
+console.log(person, "has a", event, "today at", time, "somewhere in", place);
